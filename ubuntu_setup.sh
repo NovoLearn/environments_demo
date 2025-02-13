c9413074b596a5a57b570b9e6fd6243c649ef6ed
NovoLearn <59593302+NovoLearn@users.noreply.github.com>
2025-02-13 21:49:50 +1100

    Update ubuntu_setup.sh

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #########################################
-# How to use
+# How to use #
 # Save the script: Save this bash script locally - wget https://raw.githubusercontent.com/NovoLearn/environments_demo/refs/heads/main/ubuntu_setup.sh
-# Make it executable - chmod +x setup_python.sh
-# Run the script - sudo ./setup_python.sh
+# Make it executable - chmod +x ./ubuntu_setup.sh
+# Run the script - sudo ./ubuntu_setup.sh
 #########################################
 
 
